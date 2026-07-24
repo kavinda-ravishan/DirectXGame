1b6e1ee33bbc61ba9d8e07e35be435a535c4181b
kavinda-ravishan <kvnravishan@outlook.com>
2026-07-24 20:55:42 +0530

    ignored files added

---
+++ DX3D/Source/DX3D/Game/Win32/Win32Game.cpp
@@ -0,0 +1,22 @@
+#include <Windows.h>
+#include "DX3D/Game/Game.h"
+
+void dx3d::Game::Run() {
+
+	MSG msg{};
+	while (_is_running) {
+		
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+
+			if (msg.message == WM_QUIT) {
+				_is_running = false;
+				break;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		Sleep(1);
+	}
+}
---
+++ DX3D/Source/DX3D/Window/Win32/Win32Window.cpp
@@ -0,0 +1,67 @@
+#include <Windows.h>
+#include <stdexcept>
+#include "DX3D/Window/Window.h"
+
+static LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wparam, lparam);
+	}
+
+	return 0;
+}
+
+dx3d::Window::Window(const WindowDesc& desc): Base(desc.base) {
+	
+	GetLogger().Log(Logger::LogLevel::Info, "Initializing Window");
+
+	auto RegisterWindowClass = []() {
+		WNDCLASSEX wc{};
+		wc.cbSize = sizeof(WNDCLASSEX);
+		wc.lpszClassName = L"DX3DWindow";
+		wc.lpfnWndProc = WindowProcedure;
+		return RegisterClassEx(&wc);
+	};
+	
+	static const auto window_class_id = std::invoke(RegisterWindowClass);
+
+	if (window_class_id == 0) {
+		GetLogger().Log(Logger::LogLevel::Error, "Failed to register window class");
+		throw std::runtime_error("Failed to register window class");
+	}
+
+	const DWORD window_style = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU;
+
+	RECT rc = { 0, 0, 1280, 720 };
+	AdjustWindowRect(&rc, window_style, false);
+
+	_handle = CreateWindowEx(
+		NULL, 
+		MAKEINTATOM(window_class_id), 
+		L"C++ 3D Game", 
+		window_style, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		rc.right - rc.left, 
+		rc.bottom - rc.top, 
+		NULL, 
+		NULL, 
+		NULL, 
+		NULL);
+
+	if (nullptr == _handle) {
+		GetLogger().Log(Logger::LogLevel::Error, "Failed to create window");
+		throw std::runtime_error("Failed to create window");
+	}
+
+	ShowWindow(static_cast<HWND>(_handle), SW_SHOW);
+}
+
+dx3d::Window::~Window() {
+	GetLogger().Log(Logger::LogLevel::Info, "Destroying Window");
+
+	DestroyWindow(static_cast<HWND>(_handle));
+}
